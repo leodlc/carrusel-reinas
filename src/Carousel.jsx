3c16574dbe86f9ef5bbecdc2168e75745bdae11b
leodlc <ljde@espe.edu.ec>
2023-05-11 18:16:38 -0500

    puntos añadidos

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -25,11 +25,19 @@ export const Carousel=() => {
         
 
     ];
+    const updateIndex = (newIndex) =>{
+        if(newIndex < 0){
+            newIndex=0;
+        }else if (newIndex >= items.length){
+            newIndex = items.length -1;
+        }
+        setActiveIndex(newIndex);
+    };
   return (
     <div className='carousel'>
         <div 
             className='inner'
-            style={{transform: `translate:(-${activeIndex * 100})`}}
+            style={{transform: `translate:(-${activeIndex * 100}%)`}}
         >    
         {items.map((item)=>{
             return  <CarouselItem item={item}/>;
@@ -38,18 +46,25 @@ export const Carousel=() => {
         </div>
 
         <div className = "carousel-buttons">
-            <button className='button-arrow'>
+            <button onClick={()=>{
+                updateIndex(activeIndex -1);
+            }} className='button-arrow'>
                 <span class="material-symbols-outlined">
                     arrow_back_ios
                 </span>
             </button>
             <div className='indicators'>
-                <button className='indicator-buttons'>
-                    <span class="material-symbols-outlined">
-                        radio_button_checked
-                    </span>
+                {items.map((item,index)=>{
+                    return (
+                        <button className='indicator-buttons'>
+                            <span class="material-symbols-outlined">
+                                radio_button_checked
+                            </span>
 
-                </button>
+                        </button>
+                    );
+                })}
+                
 
             </div>
             <button className='button-arrow'>
