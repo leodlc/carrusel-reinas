5f6654a2c2322fa58a8164b65cf175e9580a9dbb
DavidGLeal <adguarderas@espe.edu.ec>
2023-05-11 22:13:01 -0500

    Otro cambio jeje

--- src/CarouselItem.jsx
+++ src/CarouselItem.jsx
@@ -4,10 +4,12 @@ import "./carrusel.css"
 export const CarouselItem=( {item}) => {
   return (
     <div className='carousel-item'>
+        <div></div>
         <figure className='carousel-img'>
           <img src={item.icon.default} />
         </figure>
         <div className='carousel-item-text'>{item.description}</div>
+        <div></div>
     </div>
   );
 };
